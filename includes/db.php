477dec760e9ebcfb92fcf1215c9da52f25c49528
wadd0032 <deannawadden@rogers.com>
2012-03-05 12:44:10 -0500

    more testing

--- includes/db.php
+++ includes/db.php
@@ -6,14 +6,11 @@ require_once 'includes/filter-wrapper.php';
 //This is the best way to keep usernames and passwords out of public GitHub repos
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-$dsn = getenv('DB_DSN');
-var_dump($user);
-var_dump($pass);
-var_dump($dsn);
+$dsn = stripslashes (getenv('DB_DSN'));
 
 //Opens a connection to the database abd stores it in a variable
 //PDO allows us to connect to different types of databases, not just MySQL
-//$db = new PDO($dsn, $user, $pass);
+$db = new PDO($dsn, $user, $pass);
 
 // Makes sure we talk to the database in UTF-8, so we can support more than just English
-//$db->exec('SET NAMES utf8');
+$db->exec('SET NAMES utf8');
